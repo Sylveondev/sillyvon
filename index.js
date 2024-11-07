787dd70f420e9aae7e3475d96ccc7e3ea3aa44e3
SylveonDev <sylveondev@SylveonDevs-MBP.hsd1.nj.comcast.net>
2024-11-06 23:51:24 -0500

    Banner rotation

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const { Client, Intents, MessageEmbed, Colors, MessageActionRow, ButtonInteraction, MessageButton } = require("discord.js");
+const { Client, Intents, MessageEmbed, Colors, MessageActionRow, ButtonInteraction, MessageButton, MessageAttachment } = require("discord.js");
 const invitesTracker = require("discord-invites-tracker-sqlite");
 
 const express = require("express");
+
+const { createCanvas, loadImage } = require('@napi-rs/canvas')
 
 require("dotenv").config();
 
@@ -10,10 +12,17 @@ require("dotenv").config();
 function extension(attachment) {
   const imageLink = attachment.split(".");
   const typeOfImage = imageLink[imageLink.length - 1];
-  const image = /(jpg|jpeg|png|gif)/gi.test(typeOfImage);
+  const image = /(jpg|jpeg|png)/gi.test(typeOfImage);
   if (!image) return null;
   return attachment;
 }
+function isPng(attachment) {
+    const imageLink = attachment.split(".");
+    const typeOfImage = imageLink[imageLink.length - 1];
+    const image = /(png)/gi.test(typeOfImage);
+    if (!image) return false;
+    return true;
+  }
 
 const client = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_INVITES],
@@ -24,12 +33,52 @@ const tracker = new invitesTracker(client);
 
 client.on('ready', () => {
     console.log(`${client.user.username} is ready!`);
-    client.user.setPresence({ activities: [{ name: '!s help', type: `LISTENING` }, { name: 'invites', type: `WATCHING` }], status: 'dnd' });
+    client.user.setPresence({ activities: [{ name: ';help', type: `LISTENING` }, { name: 'invites', type: `WATCHING` }], status: 'dnd' });
+
+    setInterval(async () => {
+        try {
+            const imagehost = await client.channels.fetch('1303174589609803887');
+            const images = await imagehost.messages.fetch();
+            const pickedimage = images.at(Math.floor(Math.random() * images.size));
+            const submitter = pickedimage.mentions.users.first();
+
+            const guild = await client.guilds.fetch('1198780918681313340');
+            const channel = await guild.channels.fetch('1303168972224593952');
+            const embed = new MessageEmbed()
+            .setTitle(`Banner rotation`)
+            .setImage(pickedimage.attachments.first().url)
+            .setTimestamp(new Date())
+            .setFooter({
+                text: `Submitted by: ${submitter.tag} • ${submitter.id}`,
+                iconURL: submitter.displayAvatarURL({ dynamic: true, size: 512 })
+            })
+            .setColor("RANDOM");
+            channel.send({ embeds: [embed] });
+
+            guild.setBanner(pickedimage.attachments.first().url, `Banner rotation`)
+        }
+        catch (err) {
+            console.warn(`Silly: Failed to change banner`, err);
+        }
+    }, 3_600_000)
 
 });
 
 client.on('messageCreate', async (message) => {
     if (message.author.bot || !message.guild || message.webhookId) return;
+
+    if (message.guild.id == "1198780918681313340") {
+        if (message.channel.id == "1303221279951949845" && message.attachments.size) {
+            if (!(await extension(message.attachments.at(0).url))) return;
+            if (!await extension(message.attachments.at(0).url)) {
+                message.channel.send({ content: `${message.author}, your image isn't in the correct format. Submittions must be png, and exactly 960x540px in size.` }).then(m => setTimeout(()=>m.delete(), 5_000));
+                message.delete();
+                return;
+            }
+            message.react(`🔼`).then(()=>message.react(`🔽`));
+        }
+    }
+
     const prefix = process.env['prefix'];
     if (!message.content.startsWith(prefix)) return;
 
@@ -39,7 +88,7 @@ client.on('messageCreate', async (message) => {
     if (message.guild.id == "1198780918681313340") {
         
         if (command == "help") {
-            await message.reply(`:wave: Hi there, I'm Sillyvon. I'm a bot designed by SylveonDev for some friend servers that require it.\n\n## ${message.guild.name} specific commands:\n* \`!s staff\` : Lists the server staff\n* \`!s pingmod\` : Pings a random mod for help\n* \`!s invites\` : Shows a member's invite stats\n* \`!s invited\` : Guesses who invited a member (not very accurate)`);
+            await message.reply(`:wave: Hi there, I'm Sillyvon. I'm a bot designed by SylveonDev for some a very specific few servers that require it.\n:warning: This bot is a private bot and can only be added to whitelisted servers.\n\n## ${message.guild.name} specific commands:\n* \`;staff\` : Lists the server staff\n* \`;pingmod\` : Pings a random mod for help\n* \`;invites\` : Shows a member's invite stats\n* \`;invited\` : Guesses who invited a member\n* \`;banner\` : Creates a banner from an image`);
         }
 
         if (command == "pingmod") {
@@ -112,6 +161,40 @@ client.on('messageCreate', async (message) => {
                 if (!inviteData.invitedBy) return message.reply(`I don't know how **${member.displayName}** joined. Try looking in <#1279334497145454663> to see if it's there.`)
                 if (inviteData.invitedBy == "vanity") return message.reply(`**${member.displayName}** joined via the vanity invite.`)
                 message.reply({ content: `**${member.displayName}** was invited by <@${inviteData.invitedBy.id}>.`, allowedMentions: { parse: [] }});
+            }
+        }
+
+        if (command == "banner") {
+            try {
+                if (!message.attachments.size) return message.reply(`An image is required to use this command.`);
+                if (!(await extension(message.attachments.at(0).url))) return message.reply(`Not a valid image type.`);
+
+                const canvas = createCanvas(960, 540);
+                const ctx = canvas.getContext('2d');
+
+                const watermark = await loadImage('https://images2.imgbox.com/d2/23/aeHFKEsA_o.png');
+                const img = await loadImage(message.attachments.at(0).url);
+
+                var hRatio = canvas.width / img.width;
+                var vRatio =  canvas.height / img.height;
+                var ratio  = Math.max( hRatio, vRatio );
+                var centerShift_x = ( canvas.width - img.width*ratio ) / 2;
+                var centerShift_y = ( canvas.height - img.height*ratio ) / 2;  
+                ctx.clearRect(0,0,canvas.width, canvas.height);
+                ctx.drawImage(img, 0,0, img.width, img.height,
+                    centerShift_x,centerShift_y,img.width*ratio, img.height*ratio);
+                ctx.drawImage(watermark, 0, 0, canvas.width, canvas.height);
+                
+                const pngData = await canvas.encode('png');
+                const attachment = new MessageAttachment()
+                .setFile(pngData)
+                .setName('banner.png');
+
+                message.reply({ content: `Generated your banner. Go to <#1303221279951949845> to submit it.`, files: [attachment] });
+            }
+            catch (err) {
+                message.reply('Oops, something went wrong.');
+                console.warn(`Silly: failed to generate image`, err);
             }
         }
     }
